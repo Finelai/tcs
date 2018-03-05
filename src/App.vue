a8b712bc85ea033217bc43cd1da0e8ec28d55e04
finelai <finelai@ymail.com>
2018-03-05 17:12:48 +0300

    add CommentForm

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="header-wrapper">
+        <nav>
+          <router-link to="/">Top Comment System</router-link>
+          <router-link :to="{ name: 'ComponentForm' }">Comment Form</router-link>
+        </nav>
+      </div>
+    </header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
