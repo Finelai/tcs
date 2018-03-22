d18db746e9943db7517fa5a66c5b81b22baddc6a
finelai <finelai@ymail.com>
2018-03-22 14:39:05 +0300

    styled login with element

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="login">
+  <el-main class="login">
     <h3>Вход</h3>
-    <input type="text" v-model="email" placeholder="Email"><br>
-    <input type="password" v-model="password" placeholder="Password"><br>
-    <button v-on:click="signIn">Login</button>
-    <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
-  </div>
+    <el-form :model="inForm" :rules="rules" :status-icon="true" label-width="90px">
+      <el-form-item label="E-mail:" prop="email">
+        <el-input v-model="inForm.email"></el-input>
+      </el-form-item>
+      <el-form-item label="Пароль:" prop="password">
+        <el-input type="password" v-model="inForm.password" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="signIn">Войти</el-button>
+        <router-link to="/sign-up"><el-button>Создать</el-button></router-link>
+      </el-form-item>
+    </el-form>
+  </el-main>
 </template>
 
 <script>
@@ -14,15 +22,39 @@ import firebase from 'firebase';
 export default {
   name: 'login',
   data() {
+    const validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Введите пароль'));
+      } else if (value.length < 4) {
+        callback(new Error('Пароль должен быть длинее 3х символов'));
+      } else {
+        callback();
+      }
+    };
     return {
-      email: '',
-      password: '',
+      inForm: {
+        email: '',
+        password: '',
+      },
+      rules: {
+        email: [
+          { required: true, message: 'Укажите вашу электронную почту', trigger: 'blur' },
+          { type: 'email', message: 'Введите корректный E-mail', trigger: 'blur,change' },
+        ],
+        password: [
+          { required: true, validator: validatePass, trigger: 'blur' },
+        ],
+      },
     };
   },
   methods: {
     signIn() {
-      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+      firebase.auth().signInWithEmailAndPassword(this.inForm.email, this.inForm.password).then(
         (user) => {
+          this.$message({
+            message: `Добро пожаловать. Снова.`,
+            type: 'success',
+          });
           this.$router.push({
             name: 'UserProfile',
             params: { userId: user.uid },
@@ -30,7 +62,7 @@ export default {
         },
         (err) => {
           this.$message({
-            message: `Error: ${err.message}`,
+            message: `Ошибка входа: ${err.message}`,
             type: 'error',
           });
         },
